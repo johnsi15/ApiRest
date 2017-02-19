6346debedb19391892e6e063a2be5f4dcbae9ceb
john andrey <andrey_201015@hotmail.com>
2017-02-19 14:47:24 -0500

    Iniciando con mongoose como orm para mi base de datos mongodb

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const express = require('express')
 //parser datos para todos los request http
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,7 +13,38 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.listen(port, () => {
-  console.log('Api Rest corriendo en http://localhost:3000')
-  // console.log(`Api Rest corriendo en http://localhost:${port}`)
+// app.get('/hola', (req, res) =>{
+//  res.send({ message: 'Hola mundo'})
+// })
+
+app.get('/api/product', (req, res) =>{
+  res.send(200, { products: []})
+})
+
+app.get('/api/product/:productId', (req, res) =>{
+  
+})
+
+app.post('/api/product', (req, res) =>{
+  console.log(req.body)
+  res.status(200).send({ message: 'Se recibio el post o producto'})
+})
+
+app.put('/api/product/:productId', (req, res) =>{
+  
+})
+
+app.delete('/api/product/:productId', (req, res) =>{
+  
+})
+
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+  if (err) return console.log('Error al conectar con la base de datos: '+ err)
+  console.log('Conexión a la base de datos establecidad....');
+
+  app.listen(port, () => {
+    console.log('Api Rest corriendo en http://localhost:3000')
+    // console.log(`Api Rest corriendo en http://localhost:${port}`)
+  })
+
 })
