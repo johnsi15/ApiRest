1da79c50a1e9b30601eceda38c70d07c07250913
john andrey <andrey_201015@hotmail.com>
2017-02-26 16:47:53 -0500

    agregando modelo de productos y creando un CRUD

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const Product = require('./models/product')
+
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -18,24 +20,66 @@ app.use(bodyParser.json())
 // })
 
 app.get('/api/product', (req, res) =>{
-  res.send(200, { products: []})
+  Product.find({}, (err, products) => {
+    if(err) return res.status(500).send({ message: 'Error al realizar la peticion'+err})
+    if(!products) return res.status(404).send({ message: 'No existen products: '})
+    //mandamos solo la variable products porque es una nueva funcionalidad de EM6 para no colocar la clave: valor ya que se llaman igual
+    res.status(200).send( { products } );
+  })
 })
 
 app.get('/api/product/:productId', (req, res) =>{
-  
+  let productId = req.params.productId
+
+  Product.findById(productId, (err, product) => {
+    if(err) return res.status(500).send({ message: 'Error al realizar la peticion'+err})
+    if(!product) return res.status(404).send({ message: 'El producto no se encontro: '+prodcut})
+
+    res.status(200).send({ product: 'El producto es -> '+product })
+  })
 })
 
 app.post('/api/product', (req, res) =>{
+  console.log('POST /api/product')
   console.log(req.body)
-  res.status(200).send({ message: 'Se recibio el post o producto'})
+
+  let product = new Product()
+  product.name = req.body.name
+  product.picture = req.body.picture
+  product.price = req.body.price
+  product.category = req.body.category
+  product.description = req.body.description
+
+  product.save((err, productStored) => {
+  	if(err) res.status(500).send({ message: 'Error al salvar el producto'+ err })
+
+  	res.status(200).send({ product: productStored })
+  })
 })
 
 app.put('/api/product/:productId', (req, res) =>{
-  
+  let productId = req.params.productId
+  let update = req.body;
+
+  Product.findByIdAndUpdate(productId, update, (err, productUpdated) => {
+    if(err) return res.status(500).send({ message: 'Error al actualizar el product'+err})
+    
+    res.status(200).send({ product: productUpdated })
+  })
 })
 
 app.delete('/api/product/:productId', (req, res) =>{
-  
+  let productId = req.params.productId
+
+  Product.findById(productId, (err, product) => {
+    if(err) return res.status(500).send({ message: 'Error al eliminar el product'+err})
+    
+    product.remove(err =>{
+      if(err) return res.status(500).send({ message: 'Error al eliminar el product'+err})
+      res.status(200).send({ message: 'El producto se elimino ' })
+    })
+
+  })
 })
 
 mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
---
+++ models/product.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ProductSchema = Schema({
+  name: String,
+  picture: String,
+  price: { type: Number, default: 0 },
+  category: { type: String, enum: ['computers', 'phones', 'accesories'] },
+  description: String
+})
+
+//Indicamos el nombre que llevara como collections el documento que se crea
+module.exports = mongoose.model('Product', ProductSchema)
